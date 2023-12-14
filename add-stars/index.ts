e610c28da2123b419d2beb7121cec2c881377bcd
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-12-13 22:20:58 -0300

    add-stars working

--- add-stars/index.ts
+++ add-stars/index.ts
@@ -3,8 +3,8 @@
 // process.chdir(import.meta.dir)
 // process.chdir('..')
 
+import { execa } from "execa";
 import fastq from "fastq";
-
 import SuperExpressive from "super-expressive";
 
 const myRegexSE = SuperExpressive()
@@ -24,9 +24,17 @@ const myRegexGSE = myRegexSE.allowMultipleMatches;
 const myRegex = myRegexSE.toRegex();
 const myRegexG = myRegexGSE.toRegex();
 
+const repoRegex = SuperExpressive()
+  .string("github.com/")
+  .capture.oneOrMore.anythingButChars("/")
+  .end()
+  .string("/")
+  .capture.oneOrMore.anythingButChars("/")
+  .end()
+  .toRegex();
+
 console.log("myRegex:", myRegex);
 
-// console.log("myRegex:", myRegex)
 console.log("process.argv:", process.argv);
 const fileContent = await Bun.file(process.argv[2]).text();
 const matches = fileContent.match(myRegexG);
@@ -41,7 +49,16 @@ const queue = fastq.promise(async function (s: string) {
   if (!url.includes("github.com")) {
     return;
   }
-  const newTag = `[${name} ⭐️ 999](${url})`
+  const [, username, repoName] = url.match(repoRegex)!;
+  const { stdout: stars } = await execa("gh", [
+    "api",
+    `repos/${username}/${repoName}`,
+    "--cache",
+    `${24 * 7}h`,
+    "--jq",
+    ".stargazers_count",
+  ]);
+  const newTag = `[${name} ⭐️ ${stars}](${url})`;
   newContent = newContent.replaceAll(s, newTag);
 }, 8);
 
